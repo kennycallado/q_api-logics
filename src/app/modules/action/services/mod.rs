5233ffaf90616563c329d85e81dbdb28864ea5bd
kennycallado <kennycallado@hotmail.com>
2023-06-19 16:43:16 +0200

    UPDATE Actualización desde base

--- src/app/modules/action/services/mod.rs
+++ src/app/modules/action/services/mod.rs
@@ -1,3 +1,4 @@
 pub mod add_resource;
+pub mod recruiter;
 pub mod resource_completed;
 pub mod update_record;
--- src/app/providers/models/paper.rs
+++ src/app/providers/models/paper.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::app::providers::models::answer::PubNewAnswer;
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
 pub struct PubPaperPush {
     pub id: i32,
@@ -12,6 +12,15 @@ pub struct PubPaperPush {
     pub resource_id: i32,
     pub completed: bool,
     pub answers: Option<Vec<PubNewAnswer>>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct PubPaper {
+    pub id: i32,
+    pub project_id: i32,
+    pub resource_id: i32,
+    pub completed: bool,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
