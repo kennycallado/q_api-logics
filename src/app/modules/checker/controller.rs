8968de2d124a2fe001b3cb9a1b5777a3edd5a814
kennycallado <kennycallado@hotmail.com>
2023-06-19 16:44:26 +0200

    ADD Nuevo endpoint get for cron to hit and update the post one for clarification

--- src/app/modules/checker/controller.rs
+++ src/app/modules/checker/controller.rs
@@ -3,7 +3,7 @@ use rocket::serde::json::Json;
 use rocket::State;
 
 use crate::app::providers::guards::claims::AccessClaims;
-use crate::app::providers::models::paper::PubPaperPush;
+use crate::app::providers::models::paper::{PubPaperPush, PubPaper};
 use crate::app::providers::services::fetch::Fetch;
 
 use crate::app::modules::checker::services::helper;
@@ -12,8 +12,10 @@ pub fn routes() -> Vec<rocket::Route> {
     routes![
         options_index,
         options_show,
+        get_project_checker,
+        get_project_checker_none,
         post_project_checker,
-        post_project_checker_none
+        post_project_checker_none,
     ]
 }
 
@@ -27,11 +29,27 @@ pub fn options_show(_id: i32) -> Status {
     Status::Ok
 }
 
-#[post("/<name>", data = "<paper>", rank = 101)]
-pub async fn post_project_checker(fetch: &State<Fetch>, claims: AccessClaims, name: &str, paper: Json<PubPaperPush>) -> Result<Json<PubPaperPush>, Status> {
+#[get("/<name>/project/<id>", rank = 101)] // Maybe return just a status ?
+pub async fn get_project_checker(fetch: &State<Fetch>, claims: AccessClaims, name: &str, id: i32) -> Result<Json<Vec<PubPaper>>, Status> {
     match claims.0.user.role.name.as_str() {
-        "admin" => helper::send_to_checker(fetch, name, paper.into_inner()).await,
-        "robot" => helper::send_to_checker(fetch, name, paper.into_inner()).await,
+        "admin" => helper::prepare_and_send(fetch, claims.0.user, name, id).await,
+        _ => {
+            println!("Error: get_project_checker; Role not handled {}", claims.0.user.role.name);
+            return Err(Status::BadRequest);
+        }
+    }
+}
+
+#[get("/<_name>/project/<_id>", rank = 102)]
+pub async fn get_project_checker_none(_name: &str, _id: i32) -> Status {
+    Status::Unauthorized
+}
+
+#[post("/push", data = "<paper>", rank = 101)]
+pub async fn post_project_checker(fetch: &State<Fetch>, claims: AccessClaims, paper: Json<PubPaperPush>) -> Result<Json<PubPaperPush>, Status> {
+    match claims.0.user.role.name.as_str() {
+        "admin" => helper::send_to_checker_push(fetch, paper.into_inner()).await,
+        "robot" => helper::send_to_checker_push(fetch, paper.into_inner()).await,
         _ => {
             println!("Error: post_project_checker; Role not handled {}", claims.0.user.role.name);
             Err(Status::BadRequest)
--- src/app/modules/checker/services/helper.rs
+++ src/app/modules/checker/services/helper.rs
@@ -3,19 +3,123 @@ use rocket::State;
 use rocket::serde::json::Json;
 
 use crate::app::providers::config_getter::ConfigGetter;
-use crate::app::providers::models::paper::PubPaperPush;
+use crate::app::providers::models::paper::{PubPaperPush, PubPaper};
 use crate::app::providers::models::record::PubNewRecord;
+use crate::app::providers::services::claims::UserInClaims;
 use crate::app::providers::services::fetch::Fetch;
 
 use crate::app::modules::action::model::Action;
-use crate::app::modules::action::services::update_record;
+use crate::app::modules::action::services::{update_record, recruiter};
 use crate::app::modules::checker::model::PaperPushWithAction;
 
-pub async fn send_to_checker(fetch: &State<Fetch>, name: &str, paper: PubPaperPush) -> Result<Json<PubPaperPush>, Status> {
+pub async fn prepare_and_send(fetch: &State<Fetch>, _user: UserInClaims, name: &str, id: i32)
+-> Result<Json<Vec<PubPaper>>, Status> {
+    if name == "push" {
+        return Err(Status::BadRequest);
+    }
+
+    let paper_push = match recruiter::execute(fetch, id).await {
+        Ok(paper_push) => paper_push,
+        Err(status) => {
+            println!("Error: prepare_and_send; {}", status);
+            return Err(Status::InternalServerError);
+        }, 
+    };
+
+    // TODO: no longer true!
+    // Afeter send it, I have to update the papers
+    // because on push it's done auto but
+    // from cron it isn't
+
+    match send_to_checker_cron(fetch, name, id, paper_push).await {
+        Ok(papers) => Ok(Json(papers)),
+        Err(_) => Err(Status::InternalServerError),
+    }
+}
+
+pub async fn send_to_checker_cron(fetch: &State<Fetch>, name: &str, id: i32, papers: Vec<PubPaperPush>)
+-> Result<Vec<PubPaper>, &'static str> {
+    // type Error = &'static str;
+
     let checker_url = ConfigGetter::get_entity_url("checker")
         .unwrap_or("http://localhost:3000/checker/".to_string())
-        + name 
+        + name
         + "/project/"
+        + id.to_string().as_str();
+
+    let res;
+    {
+        let client = fetch.client.lock().await;
+        res = client
+            .post(checker_url)
+            .header("Content-Type", "application/json")
+            .json(&papers)
+            .send().await;
+    }
+
+    match res  {
+        Ok(res) => {
+            if !res.status().is_success() {
+                return Err("Error getting project lasts");
+            }
+
+            // If just to return a status
+            // all the code below can be removed
+            // but actions
+            match res.json::<Vec<PaperPushWithAction>>().await {
+                Ok(papers) => {
+                    // Execute actions
+                    let mut response_papers: Vec<PubPaper> = Vec::new();
+                    for paper in papers.iter() {
+                        let actions = paper.actions.clone();
+                        match actions {
+                            Some(actions) => {
+                                let paper_with = paper.clone().into();
+
+                                for action in actions {
+                                    match action.execute_action(fetch, &paper_with).await {
+                                        Ok(_) => (),
+                                        Err(s) => {
+                                            println!("Error executing action: {}", action.action);
+                                            println!("Error: {}", s);
+                                        },
+                                    }
+                                }
+                            },
+                            None => {},
+                        }
+
+                        // response_papers.push(PubPaperPush {
+                        //     id: paper.id,
+                        //     user_id: paper.user_id,
+                        //     user_record: paper.user_record.clone(),
+                        //     project_id: paper.project_id,
+                        //     resource_id: paper.resource_id,
+                        //     answers: paper.answers.clone(),
+                        //     completed: paper.completed,
+                        // })
+                        //
+                        response_papers.push(PubPaper {
+                            id: paper.id,
+                            project_id: paper.project_id,
+                            resource_id: paper.resource_id,
+                            completed: paper.completed,
+                        })
+                    }
+
+                    Ok(response_papers)
+                },
+                Err(_) => return Err("Error getting project lasts; Response"),
+            }
+        },
+        Err(_) => return Err("Error getting project lasts; Request"),
+    }
+}
+
+pub async fn send_to_checker_push(fetch: &State<Fetch>, paper: PubPaperPush) -> Result<Json<PubPaperPush>, Status> {
+    let checker_url = ConfigGetter::get_entity_url("checker")
+        .unwrap_or("http://localhost:3000/checker/".to_string())
+        + "push/project/"
         + paper.project_id.to_string().as_str();
 
     let res;
