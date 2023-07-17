9f74d4ea0d1f54e16f0be19fe9d14d65c44c6823
kennycallado <kennycallado@hotmail.com>
2023-07-17 16:56:32 +0200

    TEMP Those gonna be updated

--- SCRIPTS/cron_example.js
+++ SCRIPTS/cron_example.js
@@ -1,18 +1,6 @@
+const resources_mood = [ 7, 8 ];
+
 switch (user.record.step) {
-  case 1:
-    if (!user.paper.completed) {
-      if (user.record.strike) user.record.strike = user.record.strike + 1;
-      if (user.record.strike > 3) user.send_message(2);
-
-      break;
-    }
-    delete user.record.strike;
-
-    user.resource_completed(user.paper.resource_id);
-    user.add_resource(20);
-    user.record.step = 2;
-    break;
-
   case 4:
     if (!user.paper.completed) {
       if (user.record.strike) user.record.strike = user.record.strike + 1;
@@ -22,14 +10,29 @@ switch (user.record.step) {
     }
     delete user.record.strike;
 
-    user.resource_completed(user.paper.resource_id);
-    user.add_resource(50);
+    user.add_resource(6);
+
     user.record.step = 5;
     break;
 
+  case 6:
+    if (user.record.mood <= 60) { break; } // Assign a different resource ??
+
+    user.resource_completed(...resources_mood);
+    user.send_message(3);
+    user.toggle_active();
+
+    user.record.step = 7;
+    user.record.state = "landed";
+    break;
+
+  case undefined:
+    user.toggle_active();
+    user.record = { step: 1, mood: 0, state: "ignition" };
+    user.add_resource(1);
+    user.send_message(1);
+    break;
+
   default:
-    user.record.step = 1;
-    user.add_resource(10);
-    user.send_message(1)
     break;
 }
